b2314b39dfcecd764da805f54f2f1a9dd3e64042
chrisitstyle <43932836+chrisitstyle@users.noreply.github.com>
2026-06-25 17:44:07 +0200

    feat: initialize medical platform service

---
+++ services/medical-platform-service/src/main/java/com/chrisitstyle/mediscanflow/medicalplatform/MedicalPlatformServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chrisitstyle.mediscanflow.medicalplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalPlatformServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalPlatformServiceApplication.class, args);
+	}
+
+}
---
+++ services/medical-platform-service/src/test/java/com/chrisitstyle/mediscanflow/medicalplatform/MedicalPlatformServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisitstyle.mediscanflow.medicalplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalPlatformServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
